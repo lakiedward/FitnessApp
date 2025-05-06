8e6f9b65ba2104669410ead2471a50e25c13e35e
lakie <lakiedward03@gmail.com>
2025-05-07 00:18:01 +0300

    refactor: Update build configuration and dependencies for Kotlin and Compose compatibility
    Updated the build configuration and dependencies to ensure compatibility with Kotlin 1.9.22 and Compose.
    
    - Updated the Android Gradle Plugin to version 8.3.1.
    - Downgraded Kotlin version to 1.9.22.
    - Removed `kotlin-compose` plugin.
    - Updated the Compose Compiler version to 1.5.10 and added configuration to set `kotlinCompilerExtensionVersion`.
    - Updated `google-gms-google-services` plugin to 4.4.1.
    - Added note in `libs.versions.toml` about the `composeOptions` configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
 }
 
@@ -44,6 +43,9 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+             kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+        }
 }
 
 dependencies {
